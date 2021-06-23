f772baf5d80e43386d251497fdcb1dea2672b183
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2021-06-23 14:15:38 -0300

    Configure context and dependency injection
    - Add weld cdi dependency
    - Create a config class in order to use dynamic web module 4.0
    - Create a context xml file for cdi
    - Create a beans.xml file

---
+++ src/main/java/io/github/fernandoferreira/evaluation/config/Config.java
@@ -0,0 +1,10 @@
+package io.github.fernandoferreira.evaluation.config;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@FacesConfig
+@ApplicationScoped
+public class Config {
+
+}
