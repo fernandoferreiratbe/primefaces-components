6d04a297883fc043100c5c3ead334a0e8cbb1079
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2021-07-01 09:25:09 -0300

    Apply pagination for data table

--- src/main/java/io/github/fernandoferreira/evaluation/controller/StudentBean.java
+++ src/main/java/io/github/fernandoferreira/evaluation/controller/StudentBean.java
@@ -21,7 +21,9 @@ public class StudentBean {
 	public void createStudents() {
 		this.students = new ArrayList<Student>();
 		this.students.add(new Student(1L, "John Walker", "5th Avenue"));
-		this.students.add(new Student(1L, "Marilyn Monroe", "Kenedy Street"));
+		this.students.add(new Student(2L, "Marilyn Monroe", "Kenedy Street"));
+		this.students.add(new Student(3L, "Martin Luther King", "501 Auburn Ave NE"));
+		this.students.add(new Student(4L, "Malcolm X", "102 W 116th"));
 	}
 
 }
